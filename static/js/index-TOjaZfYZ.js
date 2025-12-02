987bbfebbb76ef4a7f04e116e3093822c76ff053
Lemon-Cc1 <Lemon-Cc1@users.noreply.github.com>
2025-12-02 08:50:27 +0000

    deploy: 15b928992119f2b7361293642b2b80c0e787da6a

---
+++ static/js/index-TOjaZfYZ.js
@@ -0,0 +1,1 @@
+import{d as e,c as n,o}from"./index-DZ0NoJ7K.js";const m=e({name:"Welcome",__name:"index",setup(t){return(a,c)=>(o(),n("h1",null,"Pure-Admin-Thin（非国际化版本）11"))}});export{m as default};
